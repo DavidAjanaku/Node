b62bf81541b4501aae5870672cc20a98b88cd706
David Ajanaku <user@David-Ajanaku.local>
2023-04-23 04:45:18 +0100

    learning

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+const app = express();
 
-const server = http.createServer(routes);
-server.listen(3000); 
+app.use('/product-page',(req,res,next) => {
+    console.log('in the Product Page');
+    res.send('<h1>in the Product Page </h1>')
+});
+
+app.use('/',(req,res,next) => {
+    console.log('in the another middleware');
+    res.send('<h1>Hello from here </h1>')
+})
+
+app.listen(3000);
+
+// const server = http.createServer(app);
+// server.listen(3000); 
